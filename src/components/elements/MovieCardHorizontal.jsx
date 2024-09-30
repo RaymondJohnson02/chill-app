e541c38ece31c420600d9346d28e106c512e31fd
Raymondjohnson02 <johnson.raymond2002@gmail.com>
2024-09-30 13:43:30 +0700

    hover effect

--- src/components/elements/MovieCardHorizontal.jsx
+++ src/components/elements/MovieCardHorizontal.jsx
@@ -4,9 +4,25 @@ const MovieCardHorizontal = (props) => {
     const { imageUrl, title, rating } = props;
     return(
         <a href="" className="w-full md:w-1/4 flex-shrink-0">
-        <div className={`w-full h-72 md:h-40 flex items-end justify-between bg-center bg-contain bg-no-repeat`} style={{backgroundImage: `url(${imageUrl})`}}>
-                <div className="text-white text-xl mx-5 my-16 md:my-5">{title}</div>
-                <div className="text-white text-xl mx-5 my-16 md:my-5">{rating}/5</div>
+            <div className={`group/hmovie w-full h-72 md:h-40 flex flex-row bg-center bg-contain bg-no-repeat`} style={{backgroundImage: `url(${imageUrl})`}}>
+                <div className="w-full h-full relative">
+                    <div className="w-full h-full absolute invisible group-hover/hmovie:visible transition duration-150">
+                        <div className="w-full h-full flex justify-center items-center bg-black bg-opacity-50 ">
+                            <div>
+                                <img src="/images/play-circle.png" alt="" />
+                            </div>
+                            <div>
+                                <img src="/images/add-movie.png" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full h-full absolute">
+                        <div className="w-full h-full flex justify-between items-end">
+                            <div className="text-white text-xl mx-5 my-16 md:my-5">{title}</div>
+                            <div className="text-white text-xl mx-5 my-16 md:my-5">{rating}/5</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </a>
     )
--- src/components/elements/MovieCardVertical.jsx
+++ src/components/elements/MovieCardVertical.jsx
@@ -4,7 +4,19 @@ const MovieCardVertical = (props) => {
     const { imageUrl } = props;
     return(
         <a href="" className="w-1/3 md:w-1/5 flex-shrink-0">
-            <div className={`w-full h-72 md:h-96 flex items-end justify-between bg-center bg-contain bg-no-repeat`} style={{backgroundImage: `url(${imageUrl})`}}>
+            <div className={`group/vmovie w-full h-72 md:h-96 flex items-end justify-between bg-center bg-contain bg-no-repeat`} style={{backgroundImage: `url(${imageUrl})`}}>
+                <div className="w-full h-full relative">
+                    <div className="w-full h-full absolute invisible group-hover/vmovie:visible transition duration-150">
+                        <div className="w-full h-full flex justify-center items-center bg-black bg-opacity-50 ">
+                            <div>
+                                <img src="/images/play-circle.png" alt="" />
+                            </div>
+                            <div>
+                                <img src="/images/add-movie.png" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </a>
     )
